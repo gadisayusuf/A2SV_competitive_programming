e4c4633beb2ac0d1595c4a92f4e9bfb07c546b1c
gadisayusuf <162133145+gadisayusuf@users.noreply.github.com>
2024-03-15 15:44:09 +0300

    Update Convert The Temperature.py

--- Leetcode/Convert The Temperature.py
+++ Leetcode/Convert The Temperature.py
@@ -1,1 +1,10 @@
-
+class Solution(object):
+    def convertTemperature(self, celsius):
+        """
+        :type celsius: float
+        :rtype: List[float]
+        """
+        k=celsius+273.15
+        f=celsius*1.80+32.00
+        arr=[k,f]
+        return arr
